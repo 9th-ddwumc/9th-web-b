41b99e9b3e86e04690b2f0edcf9ea75cbbbc7e3c
yeah711 <motlqkf@gmail.com>
2025-09-23 01:46:23 +0900

    feat: mission finish

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Todo from '../../9th-web-b/components/Todo';
+
+function App(): React.ReactElement {
+  return (
+    <>
+      <Todo />
+    </>
+  );
+}
+
+export default App;
---
+++ src/types/todo.ts
@@ -0,0 +1,5 @@
+export type TTodo ={
+    id: number;
+    text: string;
+};
+export default TTodo;
