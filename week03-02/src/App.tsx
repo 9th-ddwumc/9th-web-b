7337cb13deefd665d8220f2bb21e3b0ea3b2848f
Naongjin <syje1004@naver.com>
2025-09-28 22:53:38 +0900

    week03_naong_02

---
+++ week03-02/src/App.tsx
@@ -0,0 +1,13 @@
+import MoviePage from "./pages/MoviePage";
+
+function App() {
+  console.log(import.meta.env.VITE_TMDB_KEY);
+
+  return (
+    <>
+      <MoviePage />
+    </>
+  );
+}
+
+export default App;
