d82427c1f3c326b3d3cb1ad5978390e1acd87fb8
kniiiiko <dywjd1128@naver.com>
2025-09-29 12:14:28 +0900

    feat: nav feature

--- week03/src/App.tsx
+++ week03/src/App.tsx
@@ -1,10 +1,25 @@
 import { useState } from "react";
 import "./App.css";
 import MoviePage from "./pages/MoviePage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Homepage";
+import NotFoundPage from "./pages/NotFoundPage";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: "movies/:category",
+        element: <MoviePage />,
+      },
+    ],
+  },
+]);
 function App() {
-  console.log(import.meta.env.VITE_TMDB_KEY);
-  return <MoviePage></MoviePage>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
