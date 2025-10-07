4f9fb9979b3e1ec04c49c56356043954f342b83d
yeah711 <motlqkf@gmail.com>
2025-10-07 20:01:04 +0900

    feat: mission finish

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import MoviePage from "./pages/MoviePage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import MovieDetailPage from "./pages/MovieDetail";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: "movies/:category",
+        element: <MoviePage />,
+      },
+      {
+        path: "movies/:category/:id",
+        element: <MovieDetailPage />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router}></RouterProvider>;
+}
+
+export default App;
