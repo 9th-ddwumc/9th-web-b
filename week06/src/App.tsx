6a67268a71e0ed83cf4433b5d6d69a895b532321
kniiiiko <dywjd1128@naver.com>
2025-11-03 15:04:05 +0900

    feat: 실습 01

---
+++ week06/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import "./hooks/useCustomFetch";
+import { WelcomeData } from "./pages/WelcomeData";
+
+function App() {
+  return (
+    <>
+      <WelcomeData />
+    </>
+  );
+}
+
+export default App;
